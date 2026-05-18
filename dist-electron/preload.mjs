23c60abdb2ebeae4f64905a4076cb9bce44a87c8
Shara <463529650@qq.com>
2026-05-18 16:38:15 +0800

    refactor: .gitignore

--- dist-electron/preload.mjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("qualidex", {
-  getAppInfo() {
-    return electron.ipcRenderer.invoke("app:get-info");
-  }
-});
